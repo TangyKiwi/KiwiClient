bcd10b9e407d8e1d1c21a66e986fa347b4851b8b
TangyKiwi <orange.panda.65@gmail.com>
2020-12-27 21:37:37 -0800

    Add Custom Capes

---
+++ src/main/java/com/tangykiwi/kiwiclient/mixin/PlayerListEntryMixin.java
@@ -0,0 +1,30 @@
+package com.tangykiwi.kiwiclient.mixin;
+
+import com.mojang.authlib.GameProfile;
+import com.mojang.authlib.minecraft.MinecraftProfileTexture;
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
+import net.minecraft.client.network.PlayerListEntry;
+import net.minecraft.util.Identifier;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.gen.Accessor;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+import java.util.Map;
+
+@Mixin(PlayerListEntry.class)
+public abstract class PlayerListEntryMixin {
+    @Accessor
+    public abstract Map<MinecraftProfileTexture.Type, Identifier> getTextures();
+
+    @Accessor
+    public abstract GameProfile getProfile();
+
+    @Environment(EnvType.CLIENT)
+    @Inject(method = "getCapeTexture", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/network/PlayerListEntry;loadTextures()V"))
+    public void injectIntoTextures(CallbackInfoReturnable<Identifier> cir) {
+        getTextures().put(MinecraftProfileTexture.Type.CAPE, new Identifier("kiwiclient:textures/cape.png"));
+    }
+}
