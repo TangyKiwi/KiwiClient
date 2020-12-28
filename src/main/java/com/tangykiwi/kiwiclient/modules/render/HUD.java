7c9e99a91491cb7c2339e0a83126ab49cf71a255
Nicholas Zeng <taleorca@gmail.com>
2020-12-28 12:58:48 -0800

    this fixes previous commit

--- src/main/java/com/tangykiwi/kiwiclient/modules/render/HUD.java
+++ src/main/java/com/tangykiwi/kiwiclient/modules/render/HUD.java
@@ -28,34 +28,36 @@ public class HUD extends Module {
     private int ping = 0;
     private String ip = "";
 
-    // PlayerListEntry playerEntry = mc.player.networkHandler.getPlayerListEntry(mc.player.getGameProfile().getId());
     private MatrixStack matrixStack = new MatrixStack();
-
+    
     public List<String> info = new ArrayList<>();
-
+    
     public HUD() {
         super("HUD", "Shows info as an overlay", GLFW.GLFW_KEY_H, Category.RENDER);
     }
-
+    
     @Subscribe
     public void onDrawOverlay(DrawOverlayEvent e) {
         if(!mc.options.debugEnabled) {
+            TextRenderer textRenderer = mc.textRenderer;
             // info.clear();
             // // coords
             // Boolean nether = mc.world.getRegistryKey().getValue().getPath().contains("nether");
             // BlockPos pos = mc.player.getBlockPos();
             // Vec3d vec = mc.player.getPos();
             // BlockPos position = nether ? new BlockPos(vec.getX() * 8, vec.getY() * 8, vec.getZ() * 8) : new BlockPos(vec.getX() / 8, vec.getY() / 8, vec.getZ() / 8);
-
-            // // ip
+            
+            // ip
             // this.ip = mc.getCurrentServerEntry() == null ? "Singleplayer" : mc.getCurrentServerEntry().address;
             // fps
             this.fps = (mc.fpsDebugString.equals("")) ? 0 : Integer.parseInt(mc.fpsDebugString.replaceAll("[^\\d]", " ").trim().replaceAll(" +", " ").split(" ")[0]);
-
+            textRenderer.draw(e.matrix, String.format("FPS: %d", fps), 10, mc.getWindow().getScaledHeight() - 20, ColorUtil.getColorString(fps, 80, 60, 30, 15, 10, false));
+            
             // ping
-            // this.ping = playerEntry == null ? 0 : playerEntry.getLatency();
-            TextRenderer textRenderer = mc.textRenderer;
-            textRenderer.draw(e.matrix, String.format("FPS: %d", fps), 10, mc.getWindow().getScaledHeight() - 20, ColorUtil.getColorString(fps, 120, 60, 30, 15, 10));
+            PlayerListEntry playerEntry = mc.player.networkHandler.getPlayerListEntry(mc.player.getGameProfile().getId());
+            this.ping = playerEntry == null ? 0 : playerEntry.getLatency();
+            textRenderer.draw(e.matrix, String.format("Ping: %d", ping), 10, mc.getWindow().getScaledHeight() - 40, ColorUtil.getColorString(ping, 75, 180, 300, 500, 1000, true));
+
         }
     }
 }
