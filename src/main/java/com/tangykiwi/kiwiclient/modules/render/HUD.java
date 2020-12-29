20bd44f87d6c3b0603e67524712a606472598ccf
Alexander Park <parkalex380@gmail.com>
2020-12-29 11:49:26 -0600

    Added nether/overworld coordinates on the HUD with decimals

--- src/main/java/com/tangykiwi/kiwiclient/modules/render/HUD.java
+++ src/main/java/com/tangykiwi/kiwiclient/modules/render/HUD.java
@@ -27,6 +27,9 @@ public class HUD extends Module {
     private long timer = 0;
     private int ping = 0;
     private String ip = "";
+    private double altcoordx;
+    private double altcoordy;
+    private double altcoordz;
 
     private MatrixStack matrixStack = new MatrixStack();
     
@@ -42,9 +45,22 @@ public class HUD extends Module {
             TextRenderer textRenderer = mc.textRenderer;
             // info.clear();
             // // coords
-            // Boolean nether = mc.world.getRegistryKey().getValue().getPath().contains("nether");
+            Boolean nether = mc.world.getRegistryKey().getValue().getPath().contains("nether");
             // BlockPos pos = mc.player.getBlockPos();
             Vec3d vec = mc.player.getPos();
+            //int coordx, coordy, coordz = (int) vec.x, vec.y, vec.z;
+            double coordx = vec.x;
+            double coordy = vec.y;
+            double coordz = vec.z;
+            if (nether) {
+                altcoordx = coordx * 8;
+                altcoordz = coordz * 8;
+
+            } else {
+                altcoordx = coordx / 8;
+                altcoordz = coordz / 8;
+
+            }
 
             // BlockPos position = nether ? new BlockPos(vec.getX() * 8, vec.getY() * 8, vec.getZ() * 8) : new BlockPos(vec.getX() / 8, vec.getY() / 8, vec.getZ() / 8);
 
@@ -52,7 +68,7 @@ public class HUD extends Module {
             // this.ip = mc.getCurrentServerEntry() == null ? "Singleplayer" : mc.getCurrentServerEntry().address;
             // fps
             this.fps = (mc.fpsDebugString.equals("")) ? 0 : Integer.parseInt(mc.fpsDebugString.replaceAll("[^\\d]", " ").trim().replaceAll(" +", " ").split(" ")[0]);
-            textRenderer.draw(e.matrix, String.format("FPS: %d", fps), 2, mc.getWindow().getScaledHeight() - 30, ColorUtil.getColorString(fps, 80, 60, 30, 15, 10, false));
+            textRenderer.draw(e.matrix, String.format("FPS: %d", fps), 2, mc.getWindow().getScaledHeight() - 50, ColorUtil.getColorString(fps, 80, 60, 30, 15, 10, false));
 
 
             // ping
@@ -60,9 +76,17 @@ public class HUD extends Module {
             this.ping = playerEntry == null ? 0 : playerEntry.getLatency();
 
 
-            textRenderer.draw(e.matrix, "X: " + (int) vec.x + " Y: " + (int) vec.y + " Z: " + (int) vec.z, 2, mc.getWindow().getScaledHeight() - 10, ColorUtil.getColorString(ping, 75, 180, 300, 500, 1000, true));
+            textRenderer.draw(e.matrix, String.format("X: %.2f, Y: %.2f, Z: %.2f", vec.x, vec.y, vec.z), 2, mc.getWindow().getScaledHeight() - 20, ColorUtil.getColorString(ping, 75, 180, 300, 500, 1000, true));
+
+            if (nether) {
+                textRenderer.draw(e.matrix, String.format("(Overworld) X: %.2f, Y: %.2f, Z: %.2f", altcoordx, vec.y, altcoordz), 2, mc.getWindow().getScaledHeight() - 10, ColorUtil.getColorString(ping, 75, 180, 300, 500, 1000, true));
+
+            } else {
+                textRenderer.draw(e.matrix, String.format("(Nether) X: %.2f, Y: %.2f, Z: %.2f", altcoordx, vec.y, altcoordz), 2, mc.getWindow().getScaledHeight() - 10, ColorUtil.getColorString(ping, 75, 180, 300, 500, 1000, true));
+
+            }
             textRenderer.draw(e.matrix, String.format("Ping: %d", ping), 2, mc.getWindow().getScaledHeight() - 40, ColorUtil.getColorString(ping, 75, 180, 300, 500, 1000, true));
-            textRenderer.draw(e.matrix, "KiwiClient 1.0.0", 2, mc.getWindow().getScaledHeight() - 20, ColorUtil.getColorString(ping, 75, 180, 300, 500, 1000, true));
+            textRenderer.draw(e.matrix, "KiwiClient 1.0.0", 2, mc.getWindow().getScaledHeight() - 30, ColorUtil.getColorString(ping, 75, 180, 300, 500, 1000, true));
 
         }
     }
