1e0b0a9f4c2cabc4ecb2221d9ffa2aa91bcff817
TangyKiwi <orange.panda.65@gmail.com>
2020-12-29 10:17:24 -0800

    Add coords to HUD

--- src/main/java/com/tangykiwi/kiwiclient/modules/render/HUD.java
+++ src/main/java/com/tangykiwi/kiwiclient/modules/render/HUD.java
@@ -2,6 +2,7 @@ package com.tangykiwi.kiwiclient.modules.render;
 
 import com.google.common.eventbus.Subscribe;
 
+import com.tangykiwi.kiwiclient.KiwiClient;
 import com.tangykiwi.kiwiclient.modules.Module;
 import com.tangykiwi.kiwiclient.modules.Category;
 import com.tangykiwi.kiwiclient.util.ColorUtil;
@@ -38,27 +39,34 @@ public class HUD extends Module {
         if(!mc.options.debugEnabled) {
             TextRenderer textRenderer = mc.textRenderer;
             // info.clear();
-            // // coords
-            // Boolean nether = mc.world.getRegistryKey().getValue().getPath().contains("nether");
-            // BlockPos pos = mc.player.getBlockPos();
+            // coords
+            Boolean nether = mc.world.getRegistryKey().getValue().getPath().contains("nether");
             Vec3d vec = mc.player.getPos();
 
-            // BlockPos position = nether ? new BlockPos(vec.getX() * 8, vec.getY() * 8, vec.getZ() * 8) : new BlockPos(vec.getX() / 8, vec.getY() / 8, vec.getZ() / 8);
+            double altx = vec.x / 8;
+            double altz = vec.z / 8;
+
+            if(nether) {
+                altx = vec.x * 8;
+                altz = vec.z * 8;
+            }
 
             // ip
             // this.ip = mc.getCurrentServerEntry() == null ? "Singleplayer" : mc.getCurrentServerEntry().address;
             // fps
             this.fps = (mc.fpsDebugString.equals("")) ? 0 : Integer.parseInt(mc.fpsDebugString.replaceAll("[^\\d]", " ").trim().replaceAll(" +", " ").split(" ")[0]);
-            textRenderer.draw(e.matrix, String.format("FPS: %d", fps), 2, mc.getWindow().getScaledHeight() - 30, ColorUtil.getColorString(fps, 80, 60, 30, 15, 10, false));
+            textRenderer.draw(e.matrix, String.format("FPS: %d", fps), 2, mc.getWindow().getScaledHeight() - 40, ColorUtil.getColorString(fps, 80, 60, 30, 15, 10, false));
 
 
             // ping
             PlayerListEntry playerEntry = mc.player.networkHandler.getPlayerListEntry(mc.player.getGameProfile().getId());
             this.ping = playerEntry == null ? 0 : playerEntry.getLatency();
 
-            textRenderer.draw(e.matrix, "X: " + (int) vec.x + " Y: " + (int) vec.y + " Z: " + (int) vec.z, 2, mc.getWindow().getScaledHeight() - 10, 0x55FF55);
-            textRenderer.draw(e.matrix, String.format("Ping: %d", ping), 2, mc.getWindow().getScaledHeight() - 40, ColorUtil.getColorString(ping, 75, 180, 300, 500, 1000, true));
-            textRenderer.draw(e.matrix, "KiwiClient 1.0.0", 2, mc.getWindow().getScaledHeight() - 20, ColorUtil.getColorString(ping, 75, 180, 300, 500, 1000, true));
+            textRenderer.draw(e.matrix, String.format("X: %.1f Y: %.1f Z: %.1f", vec.x, vec.y, vec.z), 2, mc.getWindow().getScaledHeight() - 20, 0x55FF55);
+            if(nether) {textRenderer.draw(e.matrix, String.format("(Overworld) X: %.1f Y: %.1f Z: %.1f", altx, vec.y, altz), 2, mc.getWindow().getScaledHeight() - 10, 0x55FF55); }
+            else textRenderer.draw(e.matrix, String.format("(Nether) X: %.1f Y: %.1f Z: %.1f", altx, vec.y, altz), 2, mc.getWindow().getScaledHeight() - 10, 0x55FF55);
+            textRenderer.draw(e.matrix, String.format("Ping: %d", ping), 2, mc.getWindow().getScaledHeight() - 50, ColorUtil.getColorString(ping, 75, 180, 300, 500, 1000, true));
+            textRenderer.draw(e.matrix, KiwiClient.name + " v" + KiwiClient.version, 2, mc.getWindow().getScaledHeight() - 30, 0xFFAA00);
 
         }
     }
