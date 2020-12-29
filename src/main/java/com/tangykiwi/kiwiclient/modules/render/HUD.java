81968a8260ad7f4217539a104bb0df73a88e3e8e
Alexander Park <parkalex380@gmail.com>
2020-12-29 11:14:30 -0600

    Improved hud spacing

--- src/main/java/com/tangykiwi/kiwiclient/modules/render/HUD.java
+++ src/main/java/com/tangykiwi/kiwiclient/modules/render/HUD.java
@@ -52,7 +52,7 @@ public class HUD extends Module {
             // this.ip = mc.getCurrentServerEntry() == null ? "Singleplayer" : mc.getCurrentServerEntry().address;
             // fps
             this.fps = (mc.fpsDebugString.equals("")) ? 0 : Integer.parseInt(mc.fpsDebugString.replaceAll("[^\\d]", " ").trim().replaceAll(" +", " ").split(" ")[0]);
-            textRenderer.draw(e.matrix, String.format("FPS: %d", fps), 10, mc.getWindow().getScaledHeight() - 30, ColorUtil.getColorString(fps, 80, 60, 30, 15, 10, false));
+            textRenderer.draw(e.matrix, String.format("FPS: %d", fps), 2, mc.getWindow().getScaledHeight() - 30, ColorUtil.getColorString(fps, 80, 60, 30, 15, 10, false));
 
 
             // ping
@@ -60,9 +60,9 @@ public class HUD extends Module {
             this.ping = playerEntry == null ? 0 : playerEntry.getLatency();
 
 
-            textRenderer.draw(e.matrix, "X: " + (int) vec.x + " Y: " + (int) vec.y + " Z: " + (int) vec.z, 10, mc.getWindow().getScaledHeight() - 10, ColorUtil.getColorString(ping, 75, 180, 300, 500, 1000, true));
-            textRenderer.draw(e.matrix, String.format("Ping: %d", ping), 10, mc.getWindow().getScaledHeight() - 40, ColorUtil.getColorString(ping, 75, 180, 300, 500, 1000, true));
-            textRenderer.draw(e.matrix, "KiwiClient 1.0.0", 10, mc.getWindow().getScaledHeight() - 20, ColorUtil.getColorString(ping, 75, 180, 300, 500, 1000, true));
+            textRenderer.draw(e.matrix, "X: " + (int) vec.x + " Y: " + (int) vec.y + " Z: " + (int) vec.z, 2, mc.getWindow().getScaledHeight() - 10, ColorUtil.getColorString(ping, 75, 180, 300, 500, 1000, true));
+            textRenderer.draw(e.matrix, String.format("Ping: %d", ping), 2, mc.getWindow().getScaledHeight() - 40, ColorUtil.getColorString(ping, 75, 180, 300, 500, 1000, true));
+            textRenderer.draw(e.matrix, "KiwiClient 1.0.0", 2, mc.getWindow().getScaledHeight() - 20, ColorUtil.getColorString(ping, 75, 180, 300, 500, 1000, true));
 
         }
     }
