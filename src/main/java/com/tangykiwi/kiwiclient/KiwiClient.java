188d64b34212c0d816a35bfe19170698b993df77
TangyKiwi <orange.panda.65@gmail.com>
2021-01-19 09:49:47 -0800

    Update README & version

--- src/main/java/com/tangykiwi/kiwiclient/KiwiClient.java
+++ src/main/java/com/tangykiwi/kiwiclient/KiwiClient.java
@@ -24,7 +24,7 @@ import net.minecraft.util.math.BlockPos;
 public class KiwiClient implements ModInitializer {
 
     public static final String MOD_ID = "kiwiclient";
-    public static String name = "KiwiClient", version = "Ari1.8.21";
+    public static String name = "KiwiClient", version = "1.7.22";
 
     public static ModuleManager moduleManager;
     public static CommandManager commandManager;
