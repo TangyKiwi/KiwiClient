c31a21ebefcb7cdc026d47bbc5636376a0439b3b
TangyKiwi <orange.panda.65@gmail.com>
2021-09-16 10:36:09 -0700

    Let ESP see invis entities

---
+++ src/main/java/com/tangykiwi/kiwiclient/mixin/LivingEntityRendererMixin.java
@@ -0,0 +1,33 @@
+package com.tangykiwi.kiwiclient.mixin;
+
+import com.tangykiwi.kiwiclient.KiwiClient;
+import com.tangykiwi.kiwiclient.event.EntityRenderEvent;
+import com.tangykiwi.kiwiclient.modules.render.ESP;
+import net.minecraft.client.render.VertexConsumerProvider;
+import net.minecraft.client.util.math.MatrixStack;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.gen.Accessor;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Redirect;
+
+import net.minecraft.client.render.entity.LivingEntityRenderer;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.player.PlayerEntity;
+
+@Mixin(LivingEntityRenderer.class)
+public abstract class LivingEntityRendererMixin
+{
+    @Redirect(at = @At(value = "INVOKE",
+            target = "Lnet/minecraft/entity/LivingEntity;isInvisibleTo(Lnet/minecraft/entity/player/PlayerEntity;)Z",
+            ordinal = 0),
+            method = {
+                    "render(Lnet/minecraft/entity/LivingEntity;FFLnet/minecraft/client/util/math/MatrixStack;Lnet/minecraft/client/render/VertexConsumerProvider;I)V"})
+    private boolean canSeeEntity(LivingEntity e, PlayerEntity player)
+    {
+        if(KiwiClient.moduleManager.getModule(ESP.class).isEnabled()) {
+            return false;
+        }
+
+        return e.isInvisibleTo(player);
+    }
+}
