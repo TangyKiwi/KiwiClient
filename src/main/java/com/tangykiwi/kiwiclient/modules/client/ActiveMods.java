94fabc2cbe707b275cc550d1cab2bd1ca940b3ea
TangyKiwi <orange.panda.65@gmail.com>
2021-10-19 11:56:07 -0500

    Add disabling animation to ActiveMods

--- src/main/java/com/tangykiwi/kiwiclient/modules/client/ActiveMods.java
+++ src/main/java/com/tangykiwi/kiwiclient/modules/client/ActiveMods.java
@@ -6,6 +6,7 @@ import com.tangykiwi.kiwiclient.KiwiClient;
 import com.tangykiwi.kiwiclient.event.DrawOverlayEvent;
 import com.tangykiwi.kiwiclient.modules.Category;
 import com.tangykiwi.kiwiclient.modules.Module;
+import com.tangykiwi.kiwiclient.modules.ModuleManager;
 import com.tangykiwi.kiwiclient.util.ColorUtil;
 import com.tangykiwi.kiwiclient.util.font.GlyphPageFontRenderer;
 import com.tangykiwi.kiwiclient.util.font.IFont;
@@ -19,7 +20,8 @@ import java.util.Map;
 public class ActiveMods extends Module {
 
     public ArrayList<Module> currModules = new ArrayList<>();
-    Map<Module, Integer> changingMods = new HashMap<>();
+    Map<Module, Integer> enablingMods = new HashMap<>();
+    Map<Module, Integer> disablingMods = new HashMap<>();
     boolean firstDraw = true;
 
     public ActiveMods() {
@@ -56,38 +58,102 @@ public class ActiveMods extends Module {
                     Module enaMod = enabledMods.get(i);
                     if(!curMod.equals(enaMod)) {
                         changed = true;
-                        changingMods.put(enaMod, -textRenderer.getStringWidth(enaMod.getName()));
+                        if(disablingMods.containsKey(enaMod)) {
+                            enablingMods.put(enaMod, disablingMods.get(enaMod));
+                            disablingMods.remove(enaMod);
+                        }
+                        else {
+                            enablingMods.put(enaMod, -textRenderer.getStringWidth(enaMod.getName()));
+                        }
                         break;
                     }
                 }
                 if(!changed) {
-                    changingMods.put(enabledMods.get(currModules.size()), -textRenderer.getStringWidth(enabledMods.get(currModules.size()).getName()));
+                    Module lastMod = enabledMods.get(currModules.size());
+                    if(disablingMods.containsKey(lastMod)) {
+                        enablingMods.put(lastMod, disablingMods.get(lastMod));
+                        disablingMods.remove(lastMod);
+                    }
+                    else {
+                        enablingMods.put(enabledMods.get(currModules.size()), -textRenderer.getStringWidth(enabledMods.get(currModules.size()).getName()));
+                    }
                 }
             }
 
-            Module firstMod = enabledMods.get(0);
-            if(!firstDraw && changingMods.containsKey(firstMod)) {
-                int displace = changingMods.get(firstMod);
+            changed = false;
+            if(enabledMods.size() < currModules.size()) {
+                for(int i = 0; i < enabledMods.size(); i++) {
+                    Module curMod = currModules.get(i);
+                    Module enaMod = enabledMods.get(i);
+                    if(!curMod.equals(enaMod)) {
+                        changed = true;
+                        if(enablingMods.containsKey(curMod)) {
+                            disablingMods.put(curMod, enablingMods.get(curMod));
+                            enablingMods.remove(curMod);
+                        }
+                        else {
+                            disablingMods.put(curMod, 0);
+                        }
+                        break;
+                    }
+                }
+                if(!changed) {
+                    Module lastMod = currModules.get(enabledMods.size());
+                    if(enablingMods.containsKey(lastMod)) {
+                        disablingMods.put(lastMod, enablingMods.get(lastMod));
+                        enablingMods.remove(lastMod);
+                    }
+                    else {
+                        disablingMods.put(currModules.get(enabledMods.size()), 0);
+                    }
+                }
+            }
+
+            ArrayList<Module> display = KiwiClient.moduleManager.getEnabledMods();
+            for (Module m : disablingMods.keySet()) {
+                display.add(m);
+            }
+            display.sort(new ModuleManager.ModuleComparator());
+
+            Module firstMod = display.get(0);
+            if(!firstDraw && enablingMods.containsKey(firstMod)) {
+                int displace = enablingMods.get(firstMod);
+                DrawableHelper.fill(e.getMatrix(), displace, 60, textRenderer.getStringWidth(firstMod.getName()) + 5 + displace, 62, ColorUtil.getRainbow(4, 0.8f, 1, count * 150));
+            }
+            else if(disablingMods.containsKey(firstMod)) {
+                int displace = disablingMods.get(firstMod);
                 DrawableHelper.fill(e.getMatrix(), displace, 60, textRenderer.getStringWidth(firstMod.getName()) + 5 + displace, 62, ColorUtil.getRainbow(4, 0.8f, 1, count * 150));
             }
             else {
                 DrawableHelper.fill(e.getMatrix(), 0, 60, textRenderer.getStringWidth(firstMod.getName()) + 5, 62, ColorUtil.getRainbow(4, 0.8f, 1, count * 150));
             }
 
-            for (Module m : enabledMods) {
+            for (Module m : display) {
 
                 int offset = count * 8;
 
-                if(!firstDraw && changingMods.containsKey(m) ) {
-                    int displace = changingMods.get(m);
+                if(!firstDraw && enablingMods.containsKey(m)) {
+                    int displace = enablingMods.get(m);
                     DrawableHelper.fill(e.getMatrix(), textRenderer.getStringWidth(m.getName()) + 3 + displace, 62 + offset, textRenderer.getStringWidth(m.getName()) + 5 + displace, 62 + 8 + offset, ColorUtil.getRainbow(4, 0.8f, 1, count * 150));
                     DrawableHelper.fill(e.getMatrix(), displace, 62 + offset, textRenderer.getStringWidth(m.getName()) + 3 + displace, 62 + 8 + offset, 0x90000000);
                     textRenderer.drawString(e.getMatrix(), m.getName(), 0.2 + displace, 62.2 + offset, ColorUtil.getRainbow(4, 0.8f, 1, count * 150));
                     if(displace + 1 >= 0) {
-                        changingMods.remove(m);
+                        enablingMods.remove(m);
                     }
                     else {
-                        changingMods.replace(m, changingMods.get(m) + 1);
+                        enablingMods.replace(m, enablingMods.get(m) + 1);
+                    }
+                }
+                else if(disablingMods.containsKey(m)) {
+                    int displace = disablingMods.get(m);
+                    DrawableHelper.fill(e.getMatrix(), textRenderer.getStringWidth(m.getName()) + 3 + displace, 62 + offset, textRenderer.getStringWidth(m.getName()) + 5 + displace, 62 + 8 + offset, ColorUtil.getRainbow(4, 0.8f, 1, count * 150));
+                    DrawableHelper.fill(e.getMatrix(), displace, 62 + offset, textRenderer.getStringWidth(m.getName()) + 3 + displace, 62 + 8 + offset, 0x90000000);
+                    textRenderer.drawString(e.getMatrix(), m.getName(), 0.2 + displace, 62.2 + offset, ColorUtil.getRainbow(4, 0.8f, 1, count * 150));
+                    if(displace - 1 <= -textRenderer.getStringWidth(m.getName())) {
+                        disablingMods.remove(m);
+                    }
+                    else {
+                        disablingMods.replace(m, disablingMods.get(m) - 1);
                     }
                 }
                 else{
