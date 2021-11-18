be245f31ab39322360bd1f7cb253a56e8e404227
TangyKiwi <orange.panda.65@gmail.com>
2021-11-18 11:21:24 -0600

    Add Bhop to speed

--- src/main/java/com/tangykiwi/kiwiclient/modules/movement/Speed.java
+++ src/main/java/com/tangykiwi/kiwiclient/modules/movement/Speed.java
@@ -5,36 +5,38 @@ import com.tangykiwi.kiwiclient.event.TickEvent;
 import com.tangykiwi.kiwiclient.modules.Category;
 import com.tangykiwi.kiwiclient.modules.Module;
 import com.tangykiwi.kiwiclient.modules.settings.SliderSetting;
+import com.tangykiwi.kiwiclient.modules.settings.ToggleSetting;
+import net.minecraft.network.packet.c2s.play.ClientCommandC2SPacket;
+import net.minecraft.util.math.Vec3d;
 import org.lwjgl.glfw.GLFW;
 
 public class Speed extends Module {
 
     public Speed() {
         super("Speed", "Go fast", GLFW.GLFW_KEY_GRAVE_ACCENT, Category.MOVEMENT,
-                new SliderSetting("Speed", 0.1, 10, 2, 2));
+            new SliderSetting("Speed", 0.1, 1, 0.5, 2).withDesc("Speed ^%"),
+            new ToggleSetting("Bhop", false));
     }
 
     @Subscribe
     public void onTick(TickEvent e) {
-        double forward = mc.player.forwardSpeed;
-        double strafe = mc.player.sidewaysSpeed;
-        float yaw = mc.player.getYaw();
+        if (mc.options.keySneak.isPressed())
+            return;
 
-        double speedstrafe = getSetting(0).asSlider().getValue() / 3;
+        if ((mc.player.forwardSpeed != 0 || mc.player.sidewaysSpeed != 0)) {
+            if (!mc.player.isSprinting()) {
+                mc.player.networkHandler.sendPacket(new ClientCommandC2SPacket(mc.player, ClientCommandC2SPacket.Mode.START_SPRINTING));
+            }
 
-        if (!mc.player.isFallFlying()) {
-            if ((forward == 0.0D) && (strafe == 0.0D)) mc.player.setVelocity(0, mc.player.getVelocity().y, 0);
-            else {
-                if (forward != 0.0D) {
-                    if (strafe > 0.0D) yaw += (forward > 0.0D ? -45 : 45);
-                    else if (strafe < 0.0D) yaw += (forward > 0.0D ? 45 : -45);
+            mc.player.setVelocity(new Vec3d(0, mc.player.getVelocity().y, 0));
+            mc.player.updateVelocity(getSetting(0).asSlider().getValueFloat(),
+                    new Vec3d(mc.player.sidewaysSpeed, 0, mc.player.forwardSpeed));
 
-                    strafe = 0.0D;
-                    if (forward > 0.0D) forward = 1.0D;
-                    else if (forward < 0.0D) forward = -1.0D;
-                }
-                mc.player.setVelocity((forward * speedstrafe * Math.cos(Math.toRadians(yaw + 90.0F)) + strafe * speedstrafe * Math.sin(Math.toRadians(yaw + 90.0F))), mc.player.getVelocity().y,
-                        forward * speedstrafe * Math.sin(Math.toRadians(yaw + 90.0F)) - strafe * speedstrafe * Math.cos(Math.toRadians(yaw + 90.0F)));
+            double vel = Math.abs(mc.player.getVelocity().getX()) + Math.abs(mc.player.getVelocity().getZ());
+
+            if (getSetting(1).asToggle().state && vel >= 0.12 && mc.player.isOnGround()) {
+                mc.player.updateVelocity(vel >= 0.3 ? 0.0f : 0.15f, new Vec3d(mc.player.sidewaysSpeed, 0, mc.player.forwardSpeed));
+                mc.player.jump();
             }
         }
     }
