dff7d373c2d58527599be7874e3478bbf890889a
TangyKiwi <orange.panda.65@gmail.com>
2022-02-02 14:29:48 -0600

    Update Criticals

--- src/main/java/com/tangykiwi/kiwiclient/modules/combat/Criticals.java
+++ src/main/java/com/tangykiwi/kiwiclient/modules/combat/Criticals.java
@@ -5,6 +5,7 @@ import com.tangykiwi.kiwiclient.event.SendPacketEvent;
 import com.tangykiwi.kiwiclient.modules.Category;
 import com.tangykiwi.kiwiclient.modules.Module;
 import com.tangykiwi.kiwiclient.util.PlayerInteractEntityC2SUtils;
+import net.minecraft.entity.LivingEntity;
 import net.minecraft.network.packet.c2s.play.PlayerInteractEntityC2SPacket;
 import net.minecraft.network.packet.c2s.play.PlayerMoveC2SPacket;
 
@@ -18,16 +19,20 @@ public class Criticals extends Module {
     public void sendPacket(SendPacketEvent event) {
         if (event.getPacket() instanceof PlayerInteractEntityC2SPacket) {
             PlayerInteractEntityC2SPacket packet = (PlayerInteractEntityC2SPacket) event.getPacket();
-            if (PlayerInteractEntityC2SUtils.getInteractType(packet) == PlayerInteractEntityC2SUtils.InteractType.INTERACT_AT) this.doCritical();
+            if (PlayerInteractEntityC2SUtils.getInteractType(packet) == PlayerInteractEntityC2SUtils.InteractType.ATTACK
+                    && PlayerInteractEntityC2SUtils.getEntity(packet) instanceof LivingEntity) {
+                doCritical();
+            }
         }
     }
 
     public void doCritical() {
         if (mc.player.isInLava() || mc.player.isTouchingWater()) return;
+
         double posX = mc.player.getX();
         double posY = mc.player.getY();
         double posZ = mc.player.getZ();
-        mc.player.networkHandler.sendPacket(new PlayerMoveC2SPacket.PositionAndOnGround(posX, posY + 0.0625, posZ, true));
+        mc.player.networkHandler.sendPacket(new PlayerMoveC2SPacket.PositionAndOnGround(posX, posY + 0.0633, posZ, false));
         mc.player.networkHandler.sendPacket(new PlayerMoveC2SPacket.PositionAndOnGround(posX, posY, posZ, false));
     }
 }
