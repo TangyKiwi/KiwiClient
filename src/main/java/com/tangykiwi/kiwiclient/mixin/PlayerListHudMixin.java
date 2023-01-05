f7b22d43f991cec6d25836f0bc0a7e3ad5ca26bf
TangyKiwi <orange.panda.65@gmail.com>
2023-01-05 12:06:57 -0600

    Fix BetterTab

--- src/main/java/com/tangykiwi/kiwiclient/mixin/PlayerListHudMixin.java
+++ src/main/java/com/tangykiwi/kiwiclient/mixin/PlayerListHudMixin.java
@@ -10,11 +10,10 @@ import net.minecraft.client.network.PlayerListEntry;
 import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.text.Text;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.ModifyArg;
-import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(PlayerListHud.class)
@@ -40,11 +39,8 @@ public class PlayerListHudMixin {
         return module.isEnabled() && module.getSetting(1).asToggle().state ? width + 30 : width;
     }
 
-    @Shadow
-    protected void renderLatencyIcon(MatrixStack matrices, int width, int x, int y, PlayerListEntry entry) {}
-
-    @Redirect(method = "render", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/hud/PlayerListHud;renderLatencyIcon(Lnet/minecraft/client/util/math/MatrixStack;IIILnet/minecraft/client/network/PlayerListEntry;)V"))
-    protected void renderLatencyIcon(PlayerListHud self, MatrixStack matrices, int width, int x, int y, PlayerListEntry entry) {
+    @Inject(method = "renderLatencyIcon", at = @At("HEAD"), cancellable = true)
+    private void onRenderLatencyIcon(MatrixStack matrices, int width, int x, int y, PlayerListEntry entry, CallbackInfo info) {
         BetterTab module = (BetterTab) KiwiClient.moduleManager.getModule(BetterTab.class);
 
         if (module.isEnabled() && module.getSetting(1).asToggle().state) {
@@ -54,8 +50,7 @@ public class PlayerListHudMixin {
             int color = ColorUtil.getColorString(latency, 10, 20, 50, 75, 100, true);
             String text = latency + "ms";
             textRenderer.drawWithShadow(matrices, text, (float) x + width - textRenderer.getWidth(text), (float) y, color);
-        } else {
-            renderLatencyIcon(matrices, width, x, y, entry);
+            info.cancel();
         }
     }
 
--- src/main/java/com/tangykiwi/kiwiclient/modules/client/BetterTab.java
+++ src/main/java/com/tangykiwi/kiwiclient/modules/client/BetterTab.java
@@ -5,9 +5,11 @@ import com.tangykiwi.kiwiclient.modules.Module;
 import com.tangykiwi.kiwiclient.modules.settings.SliderSetting;
 import com.tangykiwi.kiwiclient.modules.settings.ToggleSetting;
 import net.minecraft.client.network.PlayerListEntry;
+import net.minecraft.text.MutableText;
 import net.minecraft.text.Text;
 import net.minecraft.text.TextColor;
 import net.minecraft.util.Formatting;
+import net.minecraft.world.GameMode;
 
 import java.awt.*;
 
@@ -15,24 +17,31 @@ public class BetterTab extends Module {
     public BetterTab() {
         super("BetterTab", "Tab modifications with more information", KEY_UNBOUND, Category.CLIENT,
             new SliderSetting("Tab Size", 1, 1000, 80, 0).withDesc("Changes the number of people shown in tab (default 80)"),
-            new ToggleSetting("Latency", true).withDesc("Shows ping next to player names"));
+            new ToggleSetting("Latency", true).withDesc("Shows ping next to player names"),
+            new ToggleSetting("Gamemode", true).withDesc("Shows gamemode next to player names"));
     }
 
     public Text getPlayerName(PlayerListEntry playerListEntry) {
         Text name;
-        Color color = null;
 
         name = playerListEntry.getDisplayName();
         if (name == null) name = Text.literal(playerListEntry.getProfile().getName());
 
-        if (color != null) {
-            String nameString = name.getString();
-
-            for (Formatting format : Formatting.values()) {
-                if (format.isColor()) nameString = nameString.replace(format.toString(), "");
+        if (getSetting(2).asToggle().state) {
+            GameMode gm = playerListEntry.getGameMode();
+            String gmText = "?";
+            if (gm != null) {
+                gmText = switch (gm) {
+                    case SPECTATOR -> "Sp";
+                    case SURVIVAL -> "S";
+                    case CREATIVE -> "C";
+                    case ADVENTURE -> "A";
+                };
             }
-
-            name = Text.literal(nameString).setStyle(name.getStyle().withColor(TextColor.fromRgb(color.getRGB())));
+            MutableText text = Text.literal("");
+            text.append(name);
+            text.append(" [" + gmText + "]");
+            name = text;
         }
 
         return name;
