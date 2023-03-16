7524718d9f17a105a8317403be066b19aaf4fee8
TangyKiwi <orange.panda.65@gmail.com>
2023-03-16 12:20:22 -0700

    Fix player model in Main Menu

--- src/main/java/com/tangykiwi/kiwiclient/gui/mainmenu/dummy/DummyClientPlayNetworkHandler.java
+++ src/main/java/com/tangykiwi/kiwiclient/gui/mainmenu/dummy/DummyClientPlayNetworkHandler.java
@@ -1,17 +1,27 @@
 package com.tangykiwi.kiwiclient.gui.mainmenu.dummy;
 
 import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.network.ClientDynamicRegistryType;
 import net.minecraft.client.network.ClientPlayNetworkHandler;
 import net.minecraft.client.util.telemetry.TelemetrySender;
 import net.minecraft.client.util.telemetry.WorldSession;
 import net.minecraft.network.ClientConnection;
 import net.minecraft.network.NetworkSide;
+import net.minecraft.registry.CombinedDynamicRegistries;
+import net.minecraft.registry.DynamicRegistryManager;
+import net.minecraft.registry.RegistryLoader;
+import net.minecraft.resource.LifecycledResourceManagerImpl;
+import net.minecraft.resource.ResourceType;
+import net.minecraft.resource.VanillaDataPackProvider;
 
 import java.time.Duration;
+import java.util.List;
+import java.util.stream.Stream;
 
 public class DummyClientPlayNetworkHandler extends ClientPlayNetworkHandler {
 
     private static DummyClientPlayNetworkHandler instance;
+    private CombinedDynamicRegistries<ClientDynamicRegistryType> combinedDynamicRegistries = ClientDynamicRegistryType.createCombinedDynamicRegistries();
 
     public static DummyClientPlayNetworkHandler getInstance() {
         if (instance == null) instance = new DummyClientPlayNetworkHandler();
@@ -24,5 +34,18 @@ public class DummyClientPlayNetworkHandler extends ClientPlayNetworkHandler {
 
     private DummyClientPlayNetworkHandler() {
         super(MinecraftClient.getInstance(), null, new ClientConnection(NetworkSide.CLIENTBOUND), null, MinecraftClient.getInstance().getSession().getProfile(), new WorldSession(TelemetrySender.NOOP, true, Duration.ZERO));
+        LifecycledResourceManagerImpl manager = new LifecycledResourceManagerImpl(ResourceType.SERVER_DATA, List.of(new VanillaDataPackProvider().getResourcePack()));
+
+        combinedDynamicRegistries = combinedDynamicRegistries.with(ClientDynamicRegistryType.REMOTE,
+            RegistryLoader.load(manager, combinedDynamicRegistries.getCombinedRegistryManager(), Stream.concat(
+                RegistryLoader.DYNAMIC_REGISTRIES.stream(),
+                RegistryLoader.DIMENSION_REGISTRIES.stream()
+            ).toList())
+        );
+    }
+
+    @Override
+    public DynamicRegistryManager getRegistryManager() {
+        return combinedDynamicRegistries.getCombinedRegistryManager();
     }
 }
--- src/main/java/com/tangykiwi/kiwiclient/gui/mainmenu/dummy/DummyClientWorld.java
+++ src/main/java/com/tangykiwi/kiwiclient/gui/mainmenu/dummy/DummyClientWorld.java
@@ -2,11 +2,9 @@ package com.tangykiwi.kiwiclient.gui.mainmenu.dummy;
 
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.world.ClientWorld;
-import net.minecraft.registry.*;
-
+import net.minecraft.registry.RegistryKeys;;
 import net.minecraft.world.Difficulty;
 import net.minecraft.world.World;
-import net.minecraft.world.dimension.DimensionType;
 import net.minecraft.world.dimension.DimensionTypes;
 
 public class DummyClientWorld extends ClientWorld {
@@ -14,11 +12,11 @@ public class DummyClientWorld extends ClientWorld {
     private static DummyClientWorld instance;
 
     public static DummyClientWorld getInstance() {
-        if (instance == null) instance = new DummyClientWorld();
+        if (instance == null) instance = new DummyClientWorld(DummyClientPlayNetworkHandler.getInstance());
         return instance;
     }
 
-    private DummyClientWorld() {
-        super(DummyClientPlayNetworkHandler.getInstance(), new Properties(Difficulty.EASY, false, true), World.OVERWORLD, MinecraftClient.getInstance().world.getRegistryManager().get(RegistryKeys.DIMENSION_TYPE).getEntry(MinecraftClient.getInstance().world.getDimension()), 0,0, MinecraftClient.getInstance()::getProfiler, MinecraftClient.getInstance().worldRenderer, false, 0);
+    private DummyClientWorld(DummyClientPlayNetworkHandler dummyClientPlayNetworkHandler) {
+        super(dummyClientPlayNetworkHandler, new Properties(Difficulty.EASY, false, true), World.OVERWORLD, dummyClientPlayNetworkHandler.getRegistryManager().get(RegistryKeys.DIMENSION_TYPE).entryOf(DimensionTypes.OVERWORLD), 0,0, MinecraftClient.getInstance()::getProfiler, MinecraftClient.getInstance().worldRenderer, false, 0);
     }
 }
