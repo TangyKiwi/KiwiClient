92dfd60f95e68fb2ebb9b645bb689068c2aa4b52
TangyKiwi <orange.panda.65@gmail.com>
2025-01-10 17:23:45 -0600

    Fix custom screen background rendering

--- src/main/java/com/tangykiwi/kiwiclient/gui/Base.java
+++ src/main/java/com/tangykiwi/kiwiclient/gui/Base.java
@@ -14,29 +14,29 @@ public abstract class Base extends Screen {
 
     @Override
     public void renderBackground(DrawContext drawContext, int mouseX, int mouseY, float delta) {
-        if (this.client.world != null) {
-            this.applyBlur();
-            this.renderDarkening(drawContext);
+        if (this.client.world == null) {
+            int colorOffset = (int) ((System.currentTimeMillis() / 75) % 100);
+            if (colorOffset > 50)
+                colorOffset = 50 - (colorOffset - 50);
+
+            // smooth
+            colorOffset = (int) (-(Math.cos(Math.PI * (colorOffset / 50d)) - 1) / 2 * 50);
+
+            RenderSystem.enableBlend();
+            RenderSystem.defaultBlendFunc();
+            RenderSystem.setShader(ShaderProgramKeys.POSITION_COLOR);
+
+            BufferBuilder bufferBuilder = Tessellator.getInstance().begin(VertexFormat.DrawMode.QUADS, VertexFormats.POSITION_COLOR);
+            bufferBuilder.vertex(width, 0, 0).color(80, 53, 20, 255);
+            bufferBuilder.vertex(0, 0, 0).color(80 + colorOffset / 3, 53, 20, 255);
+            bufferBuilder.vertex(0, height + 16, 0).color(159, 113, 54, 255);
+            bufferBuilder.vertex(width, height + 16, 0).color(170 + colorOffset, 103, 45, 255);
+            BufferRenderer.drawWithGlobalProgram(bufferBuilder.end());
+
+            RenderSystem.disableBlend();
         }
 
-        int colorOffset = (int) ((System.currentTimeMillis() / 75) % 100);
-        if (colorOffset > 50)
-            colorOffset = 50 - (colorOffset - 50);
-
-        // smooth
-        colorOffset = (int) (-(Math.cos(Math.PI * (colorOffset / 50d)) - 1) / 2 * 50);
-
-        RenderSystem.enableBlend();
-        RenderSystem.defaultBlendFunc();
-        RenderSystem.setShader(ShaderProgramKeys.POSITION_COLOR);
-
-        BufferBuilder bufferBuilder = Tessellator.getInstance().begin(VertexFormat.DrawMode.QUADS, VertexFormats.POSITION_COLOR);
-        bufferBuilder.vertex(width, 0, 0).color(80, 53, 20, 255);
-        bufferBuilder.vertex(0, 0, 0).color(80 + colorOffset / 3, 53, 20, 255);
-        bufferBuilder.vertex(0, height + 16, 0).color(159, 113, 54, 255);
-        bufferBuilder.vertex(width, height + 16, 0).color(170 + colorOffset, 103, 45, 255);
-        BufferRenderer.drawWithGlobalProgram(bufferBuilder.end());
-
-        RenderSystem.disableBlend();
+        this.applyBlur();
+        this.renderDarkening(drawContext);
     }
 }
