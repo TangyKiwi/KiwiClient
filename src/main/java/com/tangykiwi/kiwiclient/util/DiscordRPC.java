f3b94d1b6eda53a46667254ccb048d21494968b0
TangyKiwi <orange.panda.65@gmail.com>
2025-10-15 11:58:16 -0700

    Confirm DiscordRPC buttons

--- src/main/java/com/tangykiwi/kiwiclient/util/DiscordRPC.java
+++ src/main/java/com/tangykiwi/kiwiclient/util/DiscordRPC.java
@@ -9,6 +9,7 @@ import de.jcm.discordgamesdk.CreateParams;
 import de.jcm.discordgamesdk.LogLevel;
 import de.jcm.discordgamesdk.activity.Activity;
 import de.jcm.discordgamesdk.activity.ActivityButton;
+import de.jcm.discordgamesdk.activity.ActivityButtonsMode;
 
 public class DiscordRPC {
     public final Activity activity = new Activity();
@@ -19,13 +20,16 @@ public class DiscordRPC {
         CreateParams params = new CreateParams();
         params.setClientID(790758093113917491L);
         params.setFlags(CreateParams.getDefaultFlags());
-        activity.timestamps().setStart(Instant.now());
-        activity.addButton(new ActivityButton("Download", "https://github.com/TangyKiwi/KiwiClient"));
 
         core = new Core(params);
-        core.setLogHook(LogLevel.ERROR, (level, message) -> LOGGER.error(message));
+        core.setLogHook(LogLevel.DEBUG, (level, message) -> LOGGER.info(message));
+
+        activity.timestamps().setStart(Instant.now());
+        activity.setActivityButtonsMode(ActivityButtonsMode.BUTTONS);
+        activity.addButton(new ActivityButton("Download", "https://github.com/TangyKiwi/KiwiClient"));
         activity.assets().setLargeImage("discord_background");
         activity.setDetails("Loading");
+
         update();
     }
 
