530652a8db013f08ba435d36c4585d51b6ba2fd2
TangyKiwi <orange.panda.65@gmail.com>
2025-10-17 13:37:58 -0700

    Add custom background handling

---
+++ src/main/java/com/tangykiwi/kiwiclient/mixin/ScreenMixin.java
@@ -0,0 +1,35 @@
+package com.tangykiwi.kiwiclient.mixin;
+
+import static com.tangykiwi.kiwiclient.KiwiClient.mc;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import com.tangykiwi.kiwiclient.gui.MainMenu;
+import com.tangykiwi.kiwiclient.util.Textures;
+
+import net.minecraft.client.gl.RenderPipelines;
+import net.minecraft.client.gui.DrawContext;
+import net.minecraft.client.gui.screen.Screen;
+import net.minecraft.client.gui.screen.multiplayer.SocialInteractionsScreen;
+import net.minecraft.client.gui.screen.option.OptionsScreen;
+import net.minecraft.client.gui.screen.pack.PackScreen;
+import net.minecraft.util.Identifier;
+
+@Mixin(Screen.class)
+public class ScreenMixin {
+    @Inject(method = "renderBackgroundTexture", at = @At("HEAD"), cancellable = true)
+    private static void renderBackgroundTexture(DrawContext context, Identifier texture, int x, int y, float u, float v, int width, int height, CallbackInfo ci) {
+        if(!(mc.currentScreen instanceof PackScreen) && !(mc.currentScreen instanceof MainMenu) && !(mc.currentScreen instanceof SocialInteractionsScreen)) {            
+            ci.cancel();
+
+            if(mc.currentScreen instanceof OptionsScreen) {
+                context.drawTexture(RenderPipelines.GUI_TEXTURED, Textures.MENU3, x, y, u, v, width, height, width, height);
+            } else {
+                context.drawTexture(RenderPipelines.GUI_TEXTURED, Textures.MENU2, x, y, u, v, width, height, width, height);
+            }
+        }
+    }
+}
