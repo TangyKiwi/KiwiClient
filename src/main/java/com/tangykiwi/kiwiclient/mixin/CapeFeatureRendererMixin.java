f53a21fee007f3f823ea8e26298bb05d11a20bdb
TangyKiwi <orange.panda.65@gmail.com>
2025-11-13 14:38:04 -0800

    Add glint for Deadmau5Ears & enable cape rendering for all

--- src/main/java/com/tangykiwi/kiwiclient/mixin/CapeFeatureRendererMixin.java
+++ src/main/java/com/tangykiwi/kiwiclient/mixin/CapeFeatureRendererMixin.java
@@ -18,21 +18,21 @@ import net.minecraft.util.Identifier;
 
 @Mixin(CapeFeatureRenderer.class)
 public class CapeFeatureRendererMixin {
-    @ModifyExpressionValue(method = "render(Lnet/minecraft/client/util/math/MatrixStack;Lnet/minecraft/client/render/VertexConsumerProvider;ILnet/minecraft/client/render/entity/state/PlayerEntityRenderState;FF)V", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/render/VertexConsumerProvider;getBuffer(Lnet/minecraft/client/render/RenderLayer;)Lnet/minecraft/client/render/VertexConsumer;"))
-    private VertexConsumer modifyCapeRenderer(VertexConsumer original, MatrixStack matrixStack, VertexConsumerProvider vertexConsumerProvider, int i, PlayerEntityRenderState playerEntityRenderState, float f, float g) {
+    @ModifyExpressionValue(method = "render(Lnet/minecraft/client/util/math/MatrixStack;Lnet/minecraft/client/render/VertexConsumerProvider;ILnet/minecraft/client/render/entity/state/PlayerEntityRenderState;FF)V", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/util/SkinTextures;capeTexture()Lnet/minecraft/util/Identifier;"))
+    private Identifier enableCapeRendering(Identifier original, MatrixStack matrixStack, VertexConsumerProvider vertexConsumerProvider, int i, PlayerEntityRenderState playerEntityRenderState, float f, float g) {
         Cape cape = (Cape) KiwiClient.moduleManager.getModule(Cape.class);
         if (cape.isEnabled()) {
-            return ItemRenderer.getArmorGlintConsumer(vertexConsumerProvider, RenderLayer.getEntitySolid(cape.getCape()), cape.getSetting("Glint").asToggle().getValue());
+            return cape.getCape();
         }
         return original;
     }
 
-    // @ModifyExpressionValue(method = "render(Lnet/minecraft/client/util/math/MatrixStack;Lnet/minecraft/client/render/VertexConsumerProvider;ILnet/minecraft/client/render/entity/state/PlayerEntityRenderState;FF)V", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/util/SkinTextures;capeTexture()Lnet/minecraft/util/Identifier;"))
-    // private Identifier modifyCapeTexture(Identifier original, MatrixStack matrixStack, VertexConsumerProvider vertexConsumerProvider, int i, PlayerEntityRenderState playerEntityRenderState, float f, float g) {
-    //     Cape cape = (Cape) KiwiClient.moduleManager.getModule(Cape.class);
-    //     if (cape.isEnabled()) {
-    //         return cape.getCape();
-    //     }
-    //     return original;
-    // }
+    @ModifyExpressionValue(method = "render(Lnet/minecraft/client/util/math/MatrixStack;Lnet/minecraft/client/render/VertexConsumerProvider;ILnet/minecraft/client/render/entity/state/PlayerEntityRenderState;FF)V", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/render/VertexConsumerProvider;getBuffer(Lnet/minecraft/client/render/RenderLayer;)Lnet/minecraft/client/render/VertexConsumer;"))
+    private VertexConsumer modifyCapeRenderer(VertexConsumer original, MatrixStack matrixStack, VertexConsumerProvider vertexConsumerProvider, int i, PlayerEntityRenderState playerEntityRenderState, float f, float g) {
+        Cape cape = (Cape) KiwiClient.moduleManager.getModule(Cape.class);
+        if (cape.isEnabled()) {
+            return ItemRenderer.getArmorGlintConsumer(vertexConsumerProvider, RenderLayer.getArmorCutoutNoCull(cape.getCape()), cape.getSetting("Glint").asToggle().getValue());
+        }
+        return original;
+    }
 }
--- src/main/java/com/tangykiwi/kiwiclient/mixin/Deadmau5FeatureRendererMixin.java
+++ src/main/java/com/tangykiwi/kiwiclient/mixin/Deadmau5FeatureRendererMixin.java
@@ -5,12 +5,16 @@ import org.spongepowered.asm.mixin.injection.At;
 
 import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
 import com.tangykiwi.kiwiclient.KiwiClient;
+import com.tangykiwi.kiwiclient.module.other.Cape;
 import com.tangykiwi.kiwiclient.module.other.Deadmau5Ears;
 import com.tangykiwi.kiwiclient.util.Textures;
 
+import net.minecraft.client.render.RenderLayer;
+import net.minecraft.client.render.VertexConsumer;
 import net.minecraft.client.render.VertexConsumerProvider;
 import net.minecraft.client.render.entity.feature.Deadmau5FeatureRenderer;
 import net.minecraft.client.render.entity.state.PlayerEntityRenderState;
+import net.minecraft.client.render.item.ItemRenderer;
 import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.util.Identifier;
 
@@ -24,10 +28,11 @@ public class Deadmau5FeatureRendererMixin {
         return original;
     }
 
-    @ModifyExpressionValue(method = "render(Lnet/minecraft/client/util/math/MatrixStack;Lnet/minecraft/client/render/VertexConsumerProvider;ILnet/minecraft/client/render/entity/state/PlayerEntityRenderState;FF)V", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/util/SkinTextures;texture()Lnet/minecraft/util/Identifier;"))
-    private Identifier modifyEarTexture(Identifier original, MatrixStack matrixStack, VertexConsumerProvider vertexConsumerProvider, int i, PlayerEntityRenderState playerEntityRenderState, float f, float g) {
-        if (KiwiClient.moduleManager.getModule(Deadmau5Ears.class).isEnabled()) {
-            return Textures.EARS;
+    @ModifyExpressionValue(method = "render(Lnet/minecraft/client/util/math/MatrixStack;Lnet/minecraft/client/render/VertexConsumerProvider;ILnet/minecraft/client/render/entity/state/PlayerEntityRenderState;FF)V", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/render/VertexConsumerProvider;getBuffer(Lnet/minecraft/client/render/RenderLayer;)Lnet/minecraft/client/render/VertexConsumer;"))
+    private VertexConsumer modifyEarRenderer(VertexConsumer original, MatrixStack matrixStack, VertexConsumerProvider vertexConsumerProvider, int i, PlayerEntityRenderState playerEntityRenderState, float f, float g) {
+        Deadmau5Ears ears = (Deadmau5Ears) KiwiClient.moduleManager.getModule(Deadmau5Ears.class);
+        if (ears.isEnabled()) {
+            return ItemRenderer.getArmorGlintConsumer(vertexConsumerProvider, RenderLayer.getArmorCutoutNoCull(Textures.EARS), ears.getSetting("Glint").asToggle().getValue());
         }
         return original;
     }
