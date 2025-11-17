1496be3f48cad750956fa2fa408c6d977ec3c86f
TangyKiwi <orange.panda.65@gmail.com>
2025-11-16 22:47:01 -0800

    Update nametags and fix elytra rendering

--- src/main/java/com/tangykiwi/kiwiclient/mixin/EntityRendererMixin.java
+++ src/main/java/com/tangykiwi/kiwiclient/mixin/EntityRendererMixin.java
@@ -24,10 +24,14 @@ import net.minecraft.client.render.entity.EntityRenderDispatcher;
 import net.minecraft.client.render.entity.EntityRenderer;
 import net.minecraft.client.render.entity.state.EntityRenderState;
 import net.minecraft.client.util.math.MatrixStack;
+import net.minecraft.component.Component;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityAttachmentType;
 import net.minecraft.entity.ItemEntity;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.text.MutableText;
 import net.minecraft.text.Text;
+import net.minecraft.util.Formatting;
 import net.minecraft.util.math.Vec3d;
 
 import static com.tangykiwi.kiwiclient.KiwiClient.mc;
@@ -40,30 +44,16 @@ public class EntityRendererMixin {
 	@Shadow
    	private TextRenderer textRenderer;
 
-    // @WrapOperation(at = @At(value = "INVOKE",
-	// 	target = "Lnet/minecraft/client/render/entity/EntityRenderDispatcher;getSquaredDistanceToCamera(Lnet/minecraft/entity/Entity;)D"),
-	// 	method = "updateRenderState(Lnet/minecraft/entity/Entity;Lnet/minecraft/client/render/entity/state/EntityRenderState;F)V")
-	// private double fakeSquaredDistanceToCamera(
-	// 	EntityRenderDispatcher dispatcher, Entity entity,
-	// 	Operation<Double> original,
-	// 	@Share("actualDistanceSq") LocalDoubleRef actualDistanceSq)
-	// {
-	// 	actualDistanceSq.set(original.call(dispatcher, entity));
-		
-	// 	if(KiwiClient.moduleManager.getModule(Nametags.class).isEnabled())
-	// 		return 0;
-		
-	// 	return actualDistanceSq.get();
-	// }
-
-    // @Inject(at = @At("TAIL"),
-	// 	method = "updateRenderState(Lnet/minecraft/entity/Entity;Lnet/minecraft/client/render/entity/state/EntityRenderState;F)V")
-	// private void restoreSquaredDistanceToCamera(T entity, S state,
-	// 	float tickDelta, CallbackInfo ci,
-	// 	@Share("actualDistanceSq") LocalDoubleRef actualDistanceSq)
-	// {
-	// 	state.squaredDistanceToCamera = actualDistanceSq.get();
-	// }
+	@Inject(method = "renderLabelIfPresent", at = @At("HEAD"), cancellable = true)
+	public void renderLabelIfPresent(EntityRenderState state, Text text, MatrixStack matrices, VertexConsumerProvider vertexConsumers, int light, CallbackInfo ci) {
+		Nametags nametags = (Nametags) KiwiClient.moduleManager.getModule(Nametags.class);
+		if (nametags.isEnabled()) {
+			IEntityRenderState iState = (IEntityRenderState) state;
+			if (iState.getLabel() != null) {
+				ci.cancel();
+			}
+		}
+	}
 
 	@Inject(method = "render", at = @At("HEAD"))
 	public void render(EntityRenderState state, MatrixStack matrices, VertexConsumerProvider vertexConsumers, int light, CallbackInfo ci) {
@@ -79,17 +69,20 @@ public class EntityRendererMixin {
 	private void customRenderLabel(EntityRenderState state, Text text, MatrixStack matrices, VertexConsumerProvider vertexConsumers, int light) {
       Vec3d vec3d = ((IEntityRenderState) state).getLabelPos();
       if (vec3d != null) {
-         int i = "deadmau5".equals(text.getString()) ? -10 : 0;
-         matrices.push();
-         matrices.translate(vec3d.x, vec3d.y + 0.5, vec3d.z);
-         matrices.multiply(this.dispatcher.getRotation());
-         matrices.scale(0.025F, -0.025F, 0.025F);
-         Matrix4f matrix4f = matrices.peek().getPositionMatrix();
-         float f = (float)(-textRenderer.getWidth(text)) / 2.0F;
-         int j = (int)(mc.options.getTextBackgroundOpacity(0.25F) * 255.0F) << 24;
-         textRenderer.draw(text, f, (float)i, -2130706433, false, matrix4f, vertexConsumers, TextLayerType.SEE_THROUGH, j, light);
-		 textRenderer.draw(text, f, (float)i, -1, false, matrix4f, vertexConsumers, TextLayerType.SEE_THROUGH, 0, LightmapTextureManager.applyEmission(light, 2));
-         matrices.pop();
+        int i = "deadmau5".equals(text.getString()) ? -10 : 0;
+        matrices.push();
+        matrices.translate(vec3d.x, vec3d.y + 0.5, vec3d.z);
+        matrices.multiply(dispatcher.getRotation());
+
+		double d = Math.sqrt(state.squaredDistanceToCamera);
+		float scale = (float) Math.max(1, d / 10);
+        matrices.scale(scale * 0.025F, -scale * 0.025F, scale * 0.025F);
+        Matrix4f matrix4f = matrices.peek().getPositionMatrix();
+        float f = (float)(-textRenderer.getWidth(text)) / 2.0F;
+        int j = (int)(mc.options.getTextBackgroundOpacity(0.25F) * 255.0F) << 24;
+        textRenderer.draw(text, f, (float)i, -2130706433, false, matrix4f, vertexConsumers, TextLayerType.SEE_THROUGH, j, light);
+		textRenderer.draw(text, f, (float)i, -1, false, matrix4f, vertexConsumers, TextLayerType.SEE_THROUGH, 0, LightmapTextureManager.applyEmission(light, 2));
+        matrices.pop();
       }
    }
 
@@ -99,24 +92,16 @@ public class EntityRendererMixin {
         if (nametags.isEnabled()) {
 			IEntityRenderState iState = (IEntityRenderState) state;
 			if (EntityUtils.isPlayer(entity) && nametags.getSetting("Players").asToggle().getValue()) {
-				if (entity.getDisplayName() != null) iState.setLabel(entity.getDisplayName());
-				else iState.setLabel(entity.getName());
-				iState.setLabelPos(entity.getAttachments().getPointNullable(EntityAttachmentType.NAME_TAG, 0, entity.getLerpedYaw(tickDelta)));
+				setLabelAndPos(entity, state, tickDelta);
 			}
 			else if (EntityUtils.isAnimal(entity) && nametags.getSetting("Animals").asToggle().getValue()) {
-				if (entity.getDisplayName() != null) iState.setLabel(entity.getDisplayName());
-				else iState.setLabel(entity.getName());
-				iState.setLabelPos(entity.getAttachments().getPointNullable(EntityAttachmentType.NAME_TAG, 0, entity.getLerpedYaw(tickDelta)));
+				setLabelAndPos(entity, state, tickDelta);
 			}
 			else if (EntityUtils.isMob(entity) && nametags.getSetting("Mobs").asToggle().getValue()) {
-				if (entity.getDisplayName() != null) iState.setLabel(entity.getDisplayName());
-				else iState.setLabel(entity.getName());
-				iState.setLabelPos(entity.getAttachments().getPointNullable(EntityAttachmentType.NAME_TAG, 0, entity.getLerpedYaw(tickDelta)));
+				setLabelAndPos(entity, state, tickDelta);
 			}
 			else if (entity instanceof ItemEntity && nametags.getSetting("Items").asToggle().getValue()) {
-				if (entity.getDisplayName() != null) iState.setLabel(entity.getDisplayName());
-				else iState.setLabel(entity.getName());
-				iState.setLabelPos(entity.getAttachments().getPointNullable(EntityAttachmentType.NAME_TAG, 0, entity.getLerpedYaw(tickDelta)));
+				setLabelAndPos(entity, state, tickDelta);
 			}
 			else {
 				iState.setLabel(null);
@@ -124,4 +109,32 @@ public class EntityRendererMixin {
 			}
 		}
     }
+
+	private void setLabelAndPos(Entity entity, EntityRenderState state, float tickDelta) {
+		IEntityRenderState iState = (IEntityRenderState) state;
+		MutableText label = entity.getDisplayName() != null ? Text.literal(entity.getDisplayName().getString()) : Text.literal(entity.getName().getString());
+		if (entity instanceof LivingEntity livingEntity) {
+			double health = livingEntity.getHealth();
+			label.append(Text.literal(" ").append(String.format("%.1f", health)).formatted(getColor(health)));
+		} else if (entity instanceof ItemEntity itemEntity) {
+			int count = itemEntity.getStack().getCount();
+			label.append(Text.literal(" [x").append(Integer.toString(count)).append("]"));
+		}
+		iState.setLabel(label);
+		iState.setLabelPos(entity.getAttachments().getPointNullable(EntityAttachmentType.NAME_TAG, 0, entity.getLerpedYaw(tickDelta)));
+	}
+
+	private Formatting getColor(double health)
+	{
+		if(health <= 5)
+			return Formatting.DARK_RED;
+		
+		if(health <= 10)
+			return Formatting.GOLD;
+		
+		if(health <= 15)
+			return Formatting.YELLOW;
+		
+		return Formatting.GREEN;
+	}
 }
