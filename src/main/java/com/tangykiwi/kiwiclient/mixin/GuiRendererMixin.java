ff932f46eaa973f52aa81e8638ce0a042f4c6165
TangyKiwi <orange.panda.65@gmail.com>
2026-04-23 12:58:12 -0700

    Fix non quad rendering

--- src/main/java/com/tangykiwi/kiwiclient/mixin/GuiRendererMixin.java
+++ src/main/java/com/tangykiwi/kiwiclient/mixin/GuiRendererMixin.java
@@ -11,20 +11,20 @@ import com.mojang.blaze3d.vertex.VertexFormat;
 
 import net.minecraft.client.gui.render.GuiRenderer;
 
-// @Mixin(GuiRenderer.class)
-// public class GuiRendererMixin {
-//     @ModifyArgs(
-//         method = "render(Lnet/minecraft/client/gui/render/GuiRenderer$Draw;Lcom/mojang/blaze3d/systems/RenderPass;Lcom/mojang/blaze3d/buffers/GpuBuffer;Lcom/mojang/blaze3d/vertex/VertexFormat$IndexType;)V",
-//         at = @At(value = "INVOKE", target = "Lcom/mojang/blaze3d/systems/RenderPass;setIndexBuffer(Lcom/mojang/blaze3d/buffers/GpuBuffer;Lcom/mojang/blaze3d/vertex/VertexFormat$IndexType;)V")
-//     )
-//     private void fixNonQuadIndexing(Args args, @Local(argsOnly = true) GuiRenderer.Draw draw) {
-//         var pipeline = draw.pipeline();
-//         if (!pipeline.getLocation().getNamespace().equals("kiwiclient")) return;
+@Mixin(GuiRenderer.class)
+public class GuiRendererMixin {
+    @ModifyArgs(
+        method = "executeDraw(Lnet/minecraft/client/gui/render/GuiRenderer$Draw;Lcom/mojang/blaze3d/systems/RenderPass;Lcom/mojang/blaze3d/buffers/GpuBuffer;Lcom/mojang/blaze3d/vertex/VertexFormat$IndexType;)V",
+        at = @At(value = "INVOKE", target = "Lcom/mojang/blaze3d/systems/RenderPass;setIndexBuffer(Lcom/mojang/blaze3d/buffers/GpuBuffer;Lcom/mojang/blaze3d/vertex/VertexFormat$IndexType;)V")
+    )
+    private void fixNonQuadIndexing(Args args, @Local(argsOnly = true) GuiRenderer.Draw draw) {
+        var pipeline = draw.pipeline();
+        if (!pipeline.getLocation().getNamespace().equals("kiwiclient")) return;
 
-//         if (pipeline.getVertexFormatMode() != VertexFormat.DrawMode.QUADS) {
-//             var shapeIndexBuffer = RenderSystem.getSequentialBuffer(pipeline.getVertexFormatMode());
-//             args.set(0, shapeIndexBuffer.getIndexBuffer(draw.indexCount()));
-//             args.set(1, shapeIndexBuffer.getIndexType());
-//         }
-//     }
-// }
+        if (pipeline.getVertexFormatMode() != VertexFormat.Mode.QUADS) {
+            var shapeIndexBuffer = RenderSystem.getSequentialBuffer(pipeline.getVertexFormatMode());
+            args.set(0, shapeIndexBuffer.getBuffer(draw.indexCount()));
+            args.set(1, shapeIndexBuffer.type());
+        }
+    }
+}
