a1716538b4b76e281d0e53e16f9ec1c332fee014
TangyKiwi <orange.panda.65@gmail.com>
2026-06-15 15:27:24 -0500

    Fix InvMove

--- src/main/java/com/tangykiwi/kiwiclient/module/movement/InvMove.java
+++ src/main/java/com/tangykiwi/kiwiclient/module/movement/InvMove.java
@@ -30,20 +30,20 @@ public class InvMove extends Module {
 
     @Subscribe
     public void onTick(TickEvent event) {
-        // if (shouldInvMove(mc.screen)) {
-        //     for (KeyMapping k : new KeyMapping[] { mc.options.keyUp, mc.options.keyDown,
-        //             mc.options.keyLeft, mc.options.keyRight, mc.options.keyJump, mc.options.keyShift }) {
-        //         k.setDown(InputConstants.isKeyDown(mc.getWindow(), InputConstants.getKey(k.getName()).getValue()));
-        //     }
+        if (shouldInvMove(mc.screen)) {
+            for (KeyMapping k : new KeyMapping[] { mc.options.keyUp, mc.options.keyDown,
+                    mc.options.keyLeft, mc.options.keyRight, mc.options.keyJump, mc.options.keyShift }) {
+                k.setDown(InputConstants.isKeyDown(mc.getWindow(), InputConstants.getKey(k.saveString()).getValue()));
+            }
 
-        //     if (getSetting(0).asToggle().getValue()) {
-        //         mc.options.keyShift.setDown(InputConstants.isKeyDown(mc.getWindow(), InputConstants.getKey(mc.options.keyShift.getName()).getValue()));
-        //     }
+            if (getSetting(0).asToggle().getValue()) {
+                mc.options.keyShift.setDown(InputConstants.isKeyDown(mc.getWindow(), InputConstants.getKey(mc.options.keyShift.saveString()).getValue()));
+            }
 
-        //     if (getSetting(1).asToggle().getValue()) {
-        //         mc.options.keyJump.setDown(InputConstants.isKeyDown(mc.getWindow(), InputConstants.getKey(mc.options.keyJump.getName()).getValue()));
-        //     }
-        // }
+            if (getSetting(1).asToggle().getValue()) {
+                mc.options.keyJump.setDown(InputConstants.isKeyDown(mc.getWindow(), InputConstants.getKey(mc.options.keyJump.saveString()).getValue()));
+            }
+        }
     }
 
     private boolean shouldInvMove(Screen screen) {
