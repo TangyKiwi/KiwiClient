b699effa2d75181e5695c65934d18c5e0800ed47
TangyKiwi <orange.panda.65@gmail.com>
2026-06-14 20:50:55 -0500

    Fix Triggerbot cd

--- src/main/java/com/tangykiwi/kiwiclient/module/combat/TriggerBot.java
+++ src/main/java/com/tangykiwi/kiwiclient/module/combat/TriggerBot.java
@@ -11,6 +11,7 @@ import com.tangykiwi.kiwiclient.event.TickEvent;
 import com.tangykiwi.kiwiclient.module.Category;
 import com.tangykiwi.kiwiclient.module.Module;
 import com.tangykiwi.kiwiclient.module.setting.SliderSetting;
+import com.tangykiwi.kiwiclient.module.setting.ToggleSetting;
 
 import net.minecraft.client.gui.screens.inventory.AbstractContainerScreen;
 import net.minecraft.world.InteractionHand;
@@ -21,7 +22,9 @@ import net.minecraft.world.entity.player.Player;
 public class TriggerBot extends Module {
     public TriggerBot() {
         super("TriggerBot", "Automatically attacks the entity you are looking at", GLFW.GLFW_KEY_X, Category.COMBAT,
-            new SliderSetting("Delay", "Average random delay in milliseconds", 0, 500, 100, -1)
+            new ToggleSetting("Delay", "Enable random delay", false).withChildren(
+                new SliderSetting("Time", "Average random delay in milliseconds", 0, 500, 100, -1)
+            )
         );
     }
 
@@ -31,20 +34,25 @@ public class TriggerBot extends Module {
 
     @Subscribe
     public void onTick(TickEvent.Pre e) {
-        if (System.currentTimeMillis() - lastAttack < randomDelay) {
-            return;
+        ToggleSetting delay = getSetting(0).asToggle();
+        if (delay.getValue()) {
+            if (System.currentTimeMillis() - lastAttack < randomDelay) {
+                return;
+            }
         }
 
         Player player = mc.player;
 
-        if(player == null || !player.isAlive() || player.isSpectator() || player.getCooldowns().isOnCooldown(player.getItemInHand(InteractionHand.MAIN_HAND)) || mc.screen instanceof AbstractContainerScreen) return;
+        if(player == null || !player.isAlive() || player.isSpectator() || player.getAttackStrengthScale(1) < 1 || mc.screen instanceof AbstractContainerScreen) return;
 
         Entity target = mc.crosshairPickEntity;
         if (target == null || (target instanceof LivingEntity && ((LivingEntity) target).isDeadOrDying()) || !target.isAlive()) return;
 
         mc.gameMode.attack(player, target);
-        lastAttack = System.currentTimeMillis();
-        randomDelay = (long) (getSetting(0).asSlider().getValueD() * (1 + random.nextGaussian() * 0.18));
+        if (delay.getValue()) {
+            lastAttack = System.currentTimeMillis();
+            randomDelay = (long) (delay.getChild(0).asSlider().getValueD() * (1 + random.nextGaussian() * 0.18));
+        }
         player.swing(InteractionHand.MAIN_HAND);
     }
 }
